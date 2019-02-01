3ffec26e490e6bf7d39c7a5406e23070de94ffb8
David <jonie4@gmail.com>
2019-02-01 14:17:08 -0800

    End of lab

--- labs/lab01/lab01.py
+++ labs/lab01/lab01.py
@@ -1,16 +1,20 @@
 import Adafruit_BBIO.GPIO as GPIO
 import Adafruit_BBIO.PWM as PWM
+import Adafruit_BBIO.ADC as ADC
 
-#PWM.start("P9_42", 0) #LED-RED
-#PWM.start("P9_14", 0) #LED-GREEN
-#PWM.start("P9_16", 0) #LED-BLUE
+ADC.setup()
+
+PWM.start("P9_42", 0) #LED-RED
+PWM.start("P9_14", 0) #LED-GREEN
+PWM.start("P9_16", 0) #LED-BLUE
 #PWM.set_duty_cycle("P9_42", 1) #RED
+#PWM.set_duty_cycle("P9_42", 0) #RED
 #PWM.set_duty_cycle("P9_14", 1) #GREEN
 #PWM.set_duty_cycle("P9_14", 0) #GREEN
 #PWM.set_duty_cycle("P9_16", 1) #BLUE
 
 
-#GPIO.setup("P8_19", GPIO.IN) #BUTTON
+GPIO.setup("P8_19", GPIO.IN) #BUTTON
 #GPIO.setup("P9_36", GPIO.IN) #POT
 GPIO.setup("P9_18", GPIO.OUT) #S_DATA
 GPIO.setup("P9_22", GPIO.OUT) #S_CLOCK
@@ -37,7 +41,11 @@ GPIO.output("P9_22", GPIO.LOW) #S_CLOCK
 GPIO.output("P9_18", GPIO.HIGH) #S_DATA
 GPIO.output("P9_22", GPIO.HIGH) #S_CLOCK
 """
-	
+
+#i=2
+#while i<12 : PWM.set_duty_cycle("P9_42", not GPIO.input("P8_19")) #RED
+#while i<12 : print GPIO.input("P8_19")
+#while i<12 : print ADC.read_raw("P9_40")
 
 GPIO.cleanup()
 
