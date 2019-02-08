ac7881681ad4f2ee8a851ab87a7c9c6ed8decbc6
David <jonie4@gmail.com>
2019-02-08 10:56:01 -0800

    7 seg testing

--- labs/lab01/lab01.py
+++ labs/lab01/lab01.py
@@ -1,8 +1,11 @@
 import Adafruit_BBIO.GPIO as GPIO
 import Adafruit_BBIO.PWM as PWM
 import Adafruit_BBIO.ADC as ADC
+from Adafruit_BBIO.SPI import SPI
 
 ADC.setup()
+spi = SPI(0,0)
+#spi.writebytes([1])
 
 PWM.start("P9_42", 0) #LED-RED
 PWM.start("P9_14", 0) #LED-GREEN
@@ -25,28 +28,12 @@ segments = [ 0xC0, 0xF9, 0xA4, 0xB0, 0x99, 0x92, 0x82, 0xF8, 0x80, 0x90, 0x88, 0
 
 
 #7-segment writing
+spi.writebytes(segments[2])
 
-GPIO.output("P9_15", GPIO.HIGH) #S_CLEAR
-GPIO.output("P9_15", GPIO.LOW) #S_CLEAR
-GPIO.output("P9_17", GPIO.LOW) #S_LATCH
-
-"""
-for i in range(8):
-	GPIO.output("P9_18", GPIO.HIGH) #S_DATA
-	GPIO.output("P9_22", GPIO.HIGH) #S_CLOCK
-	GPIO.output("P9_22", GPIO.LOW) #S_CLOCK
-GPIO.output("P9_17", GPIO.HIGH) #S_LATCH
-"""
-
-"""
-GPIO.output("P9_22", GPIO.LOW) #S_CLOCK
-GPIO.output("P9_18", GPIO.HIGH) #S_DATA
-GPIO.output("P9_22", GPIO.HIGH) #S_CLOCK
-"""
 
 #while i<12 : PWM.set_duty_cycle("P9_42", not GPIO.input("P8_19")) #RED
 #while i<12 : print GPIO.input("P8_19")
-while 1 : print ADC.read_raw("P9_36")
+#while 1 : print ADC.read_raw("P9_36")
 #while 1 : print ADC.read("P9_36")
 
 GPIO.cleanup()
