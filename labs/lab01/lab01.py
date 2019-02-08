24bb47c17fef99f53f0955f4c4d69717ab726cce
David <jonie4@gmail.com>
2019-02-08 11:22:45 -0800

    fixed invert syntax

--- labs/lab01/lab01.py
+++ labs/lab01/lab01.py
@@ -28,7 +28,7 @@ segments = [ [0xC0], [0xF9], [0xA4], [0xB0], [0x99], [0x92], [0x82], [0xF8], [0x
 def part01() :
 	while 1 :
 		time.sleep(0.1)
-		if( !GPIO.input("P8_19") )
+		if not(GPIO.input("P8_19") )
 			num++
 		spi.writebytes(segments[ num%16 ] )
 		num = num % 16
